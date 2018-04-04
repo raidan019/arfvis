8869e9cdd812e8ec4341a6acab3c51fcc8278e07
Stokes <x92443@usmawkddth17613.eecs.usma.edu>
2018-04-03 22:51:16 -0400

    Models.py Updated

--- arCode/arfvis/rf/models.py
+++ arCode/arfvis/rf/models.py
@@ -3,25 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Sensor(models.Model):
+    sensor_id = models.IntegerField(default=0)
     antenna_type = models.IntegerField(default=0)
     antenna_direction = models.IntegerField(default=0)
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
     elevation = models.IntegerField(default=0)
+    ssid = models.IntegerField(default=0)
+    encryption_type = models.IntegerField(default=0)
+    modulation = models.IntegerField(default=0)
 
 class Signal(models.Model):
-    modulation = models.IntegerField(default=0)
+    sample_id = models.IntegerField(default=0)
+    azimuth = models.FloatField(default=0.0)
+    signal_strength = models.IntegerField(default = 0)
+    sensor_latitude = models.FloatField(default=0.0)
+    sensor_longitude = models.FloatField(default=0.0)
+    modulation = models.IntegerField(default = 0)
+    encryption_type = models.IntegerField(default = 0)
+    meter_distance = models.IntegerField(default = 0)
     power = models.IntegerField(default = 0)
-    bssid = models.CharField(max_length=17) #12 with no colons. Allowing colons
-    encryption = models.IntegerField(default = 0)
-    channel = models.IntegerField(default = 0)
-    essid = models.CharField(max_length=255) 
+    date_time = models.DateTimeField(auto_now_add=True, blank=True) 
 
 class Modulation(models.Model):
-    modulation_id = models.IntegerField(default=0)
+    modulation_id = models.ForeignKey(modulation)
     modulation_name = models.CharField(max_length=100)
 
 class Encryption(models.Model):
-    encryption_id = models.IntegerField(default=0)
+    encryption_id = models.ForeignKey(encryption_type)
     encryption_name = models.CharField(max_length=100)
+    key_size = models.IntegerField(default = 0)
 
