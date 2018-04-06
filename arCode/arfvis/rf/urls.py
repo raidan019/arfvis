3fc9bef7cf9b6753477a4ba650c4bc0a75d087a2
Stokes <x92443@usmawkddth17613.eecs.usma.edu>
2018-04-06 09:32:40 -0400

    Updated Views

--- arCode/arfvis/rf/urls.py
+++ arCode/arfvis/rf/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import url, paths
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
