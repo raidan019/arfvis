fec4a1da68df25ac3d1b89bbc42ff647a85e5a02
Stokes <x92443@usmawkddth17618.eecs.usma.edu>
2018-04-12 09:14:21 -0400

    Models Corrected

--- arCode/arfvis/rf/models.py
+++ arCode/arfvis/rf/models.py
@@ -14,7 +14,7 @@ class Sensor(models.Model):
     modulation = models.IntegerField(default=0)
 
 class Signal(models.Model):
-    sample_id = models.ForeignKey(sensor_id)
+    sample_id = models.ForeignKey(Sensor, on_delete=models.CASCADE, default=1)
     azimuth = models.FloatField(default=0.0)
     signal_strength = models.IntegerField(default = 0)
     sensor_latitude = models.FloatField(default=0.0)
@@ -26,11 +26,11 @@ class Signal(models.Model):
     date_time = models.DateTimeField(auto_now_add=True, blank=True) 
 
 class Modulation(models.Model):
-    modulation_id = models.ForeignKey(modulation)
+    modulation_id = models.ForeignKey(Signal, on_delete=models.CASCADE, default=1)
     modulation_name = models.CharField(max_length=100)
 
 class Encryption(models.Model):
-    encryption_id = models.ForeignKey(encryption_type)
+    encryption_id = models.ForeignKey(Signal, on_delete=models.CASCADE, default=1)
     encryption_name = models.CharField(max_length=100)
     key_size = models.IntegerField(default = 0)
 
--- arCode/arfvis/rf/urls.py
+++ arCode/arfvis/rf/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.conf.urls import url, paths
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    path('admin/', admin.site.urls),
 ]
