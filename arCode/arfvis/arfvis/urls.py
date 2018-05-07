884e11a4554120a641028d39e4280a516ff1cfcc
Bondoc <x93146@usmawkddth17611.eecs.usma.edu>
2018-05-07 08:41:59 -0400

    updated urls.py

--- arCode/arfvis/arfvis/urls.py
+++ arCode/arfvis/arfvis/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
-    url(r'^rf/', include('rf.urls')),
-    url(r'^admin/', admin.site.urls),
+    
+	url(r'', include('rf.urls')),
+	path('accounts/', include('django.contrib.auth.urls')),
 ]
--- arCode/arfvis/rf/urls.py
+++ arCode/arfvis/rf/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 '''
