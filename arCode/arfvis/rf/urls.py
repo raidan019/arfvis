3a37efedd0c0508d6100a83a2a663156d825ee25
Bondoc <x93146@usmawkddth17611.eecs.usma.edu>
2018-05-07 09:21:22 -0400

    added upload_pic.htmml

--- arCode/arfvis/rf/urls.py
+++ arCode/arfvis/rf/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
      path('hololens', views.hololens, name = 'hololens'),
      path('upload_pic', views.upload_pic, name = 'upload_pic'),
      path('<int:signal_id>/', views.detail, name = 'detail'),
-<<<<<<< HEAD
      path('addsensor', views.addsensor, name = 'addsensor'),
-=======
-     path('addsensor', views.addsensor, name = 'addsensor')
->>>>>>> 0168b3030e7111742c985a3d8f3572e0206ac0b9
+
 ]
